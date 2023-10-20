0936c4ef06a59f31fe737cc9b173066057f1ac10
fborjaz <fborjaz@unemi.edu.ec>
2023-10-19 23:54:35 -0500

    update main

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 # menu.py
+import os
+import time
 import function
 
 while True:
+    os.system('clear' if os.name == 'posix' else 'cls')  # Limpiar la pantalla (funciona en sistemas Unix y Windows)
     print("Elija un ejercicio:")
     print("1. Calcular suma de una lista de números")
     print("2. Encontrar el número más grande en una lista de números")
-
     print("0. Salir")
 
-    opcion = int(input("Ingrese el número del ejercicio (0 para salir): "))
+    try:
+        opcion = int(input("Ingrese el número del ejercicio (0 para salir): "))
 
-    if opcion == 0:
-        break
-    elif opcion == 1:
-        lista = [int(x) for x in input("Ingrese la lista de números separados por espacios: ").split()]
-        resultado = funciones.calcular_suma(lista)
-        print("El resultado es:", resultado)
-    elif opcion == 2:
-        lista = [int(x) for x in input("Ingrese la lista de números separados por espacios: ").split()]
-        resultado = funciones.encontrar_maximo(lista)
-        print("El número más grande es:", resultado)
+        if opcion == 0:
+            break
+        elif opcion == 1:
+            lista = [int(x) for x in input("Ingrese la lista de números separados por espacios: ").split()]
+            resultado = function.calcular_suma(lista)
+            print("El resultado es:", resultado)
+        elif opcion == 2:
+            lista = [int(x) for x in input("Ingrese la lista de números separados por espacios: ").split()]
+            resultado = function.encontrar_maximo(lista)
+            print("El número más grande es:", resultado)
+        else:
+            print("Opción no válida. Por favor, elija una opción válida.")
+
+        time.sleep(5)  # Espera 5 segundos
+    except ValueError:
+        print("Entrada no válida. Por favor, ingrese un número válido.")
+        time.sleep(5)  # Espera 5 segundos
