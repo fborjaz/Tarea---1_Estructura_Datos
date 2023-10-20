930a2e13bfbd90ce1ab590236fb510b308367e72
fborjaz <fborjaz@unemi.edu.ec>
2023-10-19 23:45:57 -0500

    index

---
+++ index.py
@@ -0,0 +1,14 @@
+# resolucion.py
+import function
+
+# Ejercicio 1
+lista = [1, 2, 3, 4, 5]
+resultado = function.calcular_suma(lista)
+print("Ejercicio 1: La suma de la lista es", resultado)
+
+# Ejercicio 2
+lista = [10, 5, 8, 20, 3]
+resultado = function.encontrar_maximo(lista)
+print("Ejercicio 2: El número más grande es", resultado)
+
+# Agrega la resolución del resto de los ejercicios aquí...
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # menu.py
-import funciones
+import function
 
 while True:
     print("Elija un ejercicio:")
     print("1. Calcular suma de una lista de números")
     print("2. Encontrar el número más grande en una lista de números")
-    # Agrega las demás opciones aquí...
+
     print("0. Salir")
 
     opcion = int(input("Ingrese el número del ejercicio (0 para salir): ")
@@ -20,4 +20,3 @@ while True:
         lista = [int(x) for x in input("Ingrese la lista de números separados por espacios: ").split()]
         resultado = funciones.encontrar_maximo(lista)
         print("El número más grande es:", resultado)
-# Agrega el resto de las opciones aquí...
